4426589c226a02b3e773942db788e21cc54d1250
yukun <15152347277@163.com>
2018-02-14 17:11:44 +0800

    配置文件

--- config.py
+++ config.py
@@ -4,5 +4,18 @@ SITE_TITLE = 'Quiet 个人博客'
 # 站点副标题
 SITE_SUBTITLE = '沧浪之水清兮，可以濯吾缨，沧浪之水浊兮，可以濯吾足。'
 
+# 放置 md 文件夹
+# 文章 md 文件夹
+POST_PATH = './source/_posts/'
+# 页面 md 文件夹
+PAGE_PATH = './source/_pages/'
+# 输出 html 文件夹
+GENERATED_PATH = './quiet/static/generated/'
+# 默认分类
+DEFAULT_CATEGORY = '未分类'
+# 默认标签
+DEFAULT_TAG = ['其他']
 
+# 存放 shelve 数据文件
+BLOG_DAT = './quiet/static/generated/data.dat'
 
